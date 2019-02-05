2655f2730ae61d366fded5c11a8f48f11816491a
darius-despain <darius.despain@gmail.com>
2019-02-04 20:10:38 -1000

    Attempt 1 success after DNF

---
+++ ziplist.js
@@ -0,0 +1,23 @@
+function zipList(list1, list2) {
+  const newList = [];
+  let j = 0;
+  for (let i = 0; i < (2 * list1.length); i++) {
+    if (i % 2 === 0) {
+      newList[i] = list1[j];
+    } else {
+      newList[i] = list2[j];
+      j++;
+    }
+  }
+  return newList;
+}
+
+console.log(zipList([1, 2, 3], ['a', 'b', 'c']));
+
+function zipListTheSimpleWay(list1, list2) {
+  let newList = [];
+  newList = _.zip(list1, list2);
+  return _.flatten(newList);
+}
+
+console.log(zipListTheSimpleWay([1, 2, 3], ['a', 'b', 'c']));
